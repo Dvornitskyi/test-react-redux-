c501bbb871e4f6d02e33cef2716dcd9ee0c57dc2
Yan Dvornitskyi <dvornitskyi@gmail.com>
2022-01-24 13:22:27 +0300

    add: store and composeWithDevTools

--- redux1/src/index.js
+++ redux1/src/index.js
@@ -2,26 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from "redux";
-import { Provider } from 'react-redux'
-
-const defaultState = {
-  cash: 0,
-}
-
-const reducer = (state = defaultState , action) => {
-  switch(action.type){
-    case "ADD_CASH":
-      return {...state, cash: state.cash + action.payload}
-
-    case "GET_CASH":  
-    return {...state, cash: state.cash - action.payload}
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer);
+import {Provider} from 'react-redux'
+import {store} from "./store"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -32,7 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+
